03353df7d4c3d50624df671bc7848a3af1e2cebc
Marta Sleboda <sleboda.m98@gmail.com>
2022-01-21 19:07:23 +0100

    if statement

--- Javascript/Love calculator/script.js
+++ Javascript/Love calculator/script.js
@@ -1,5 +1,14 @@
 function loveCalculator() {
     var loveScore = Math.random();
     loveScore = Math.floor(loveScore * 100) + 1;
-    alert("Your love score is " + loveScore + "% !");
+
+    if (loveScore > 75) {
+        alert("Your love score is " + loveScore + "% !" + " Being together is your destiny!");
+    } else if (loveScore > 60) {
+        alert("Your love score is " + loveScore + "% !" + " You're a cute couple.");
+    } else if (loveScore > 40) {
+        alert("Your love score is " + loveScore + "% !" + " You're cute together... as friends.");
+    } else {
+        alert("Your love score is " + loveScore + "% !" + " Forget about it...");
+    }
 }
