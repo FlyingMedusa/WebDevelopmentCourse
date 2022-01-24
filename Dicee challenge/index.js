716a1096b3415f5d9f74809177a8dea67c6663c7
Marta Sleboda <sleboda.m98@gmail.com>
2022-01-24 22:20:11 +0100

    showing winner

--- Dicee challenge/index.js
+++ Dicee challenge/index.js
@@ -5,3 +5,13 @@ var randomNumber2 = diceImg[Math.floor(Math.random() * 6)];
 
 document.querySelector(".img1").setAttribute("src", randomNumber1);
 document.querySelector(".img2").setAttribute("src", randomNumber2);
+
+//(document.getElementById("title").textContent === "Refresh Me") 
+
+if (randomNumber1 > randomNumber2) {
+    document.getElementById("title").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.getElementById("title").innerHTML = "Player 2 Wins!🚩";
+} else {
+    document.getElementById("title").innerHTML = "Draw!";
+}
