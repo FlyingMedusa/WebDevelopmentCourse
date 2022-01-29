fc3841298b8bee387ad07cb5532207463e0a3593
Marta Sleboda <sleboda.m98@gmail.com>
2022-01-29 22:27:06 +0100

    sounds - show and user click

--- Simon game/game.js
+++ Simon game/game.js
@@ -1,42 +1,29 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+
+    playSound(userChosenColour);
+});
 
 function nextSequence() {
     var randomChosenColour = buttonColours[Math.floor(Math.random() * 4)];
     gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
 }
 
-nextSequence()
-$("#"+gamePattern[0]).fadeOut(100).fadeIn(100);
-
-
-makeSound(gamePattern[0]);
-
-function makeSound(color) {
-
-    switch (color) {
-        case "red":
-            var col1 = new Audio('sounds/red.mp3');
-            col1.play();
-        break;
-
-        case "blue":
-            var col2 = new Audio('sounds/blue.mp3');
-            col2.play();
-        break;
-
-        case "green":
-            var col3 = new Audio('sounds/green.mp3');
-            col3.play();
-        break;
-
-        case "yellow":
-            var col4 = new Audio('sounds/yellow.mp3');
-            col4.play();
-        break;
-
-        default:
-            console.log(gamePattern[0]);
-    }
+function playSound(name) {
+    var audio = new Audio("sounds/" + name +".mp3");
+    audio.play();
 }
+
+nextSequence();
+
